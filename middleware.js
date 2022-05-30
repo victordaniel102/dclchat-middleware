21cf20dfd6c8ee7060c49bb6b9ee0e3d4220a19d
LuSizilio <lucianorsizilio@gmail.com>
2022-05-29 23:34:40 -0300

    update middleware

--- middleware.js
+++ middleware.js
@@ -5,29 +5,17 @@ var express = require('express');
 var socket = require('socket.io');
 var app = express();
 
-var client = new net.Socket();
-var backendIp = 'IP_ADDRESS';
+var backendIp = '192.168.0.10';
 var backendPort = 13000;
-client.connect(backendPort, backendIp, function() {
-	console.log('Connected');
-	client.write(JSON.stringify({
-		eventName: "testando evento",
-		socketId: "testando socketId",
-		name: "luciano"
-	}));
-});
 
-client.on('data', function(data) {
-	console.log('Received: ' + data);
-	client.destroy(); // kill client after server's response
-});
 
-client.on('close', function() {
-	console.log('Connection closed');
-});
+function formatData(data){
+	let result = Buffer.from(data).toString().split('endMessageDCL');
+	result.pop();
+	return result;
+}
 
-var users = {};
-var server = app.listen(process.env.PORT || 13000, function(){
+var server = app.listen(process.env.PORT || 13001, function(){
 	console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 app.use(express.static('public'));
@@ -40,34 +28,115 @@ var io = socket(server, {
     }
 });
 
-io.on('connection', (socket) => {
+io.on('connection', async (socket) => {
+	var clientTcp = new net.Socket();
+	console.log('teste')
+	const ids = await io.allSockets();
+	console.log(ids,'ids')
+
+	clientTcp.connect(backendPort, backendIp, function() {
+		console.log('== Conectado com servidor TCP ==');
+	});
+	
+	clientTcp.on('data', function(data) {
+		data = formatData(data);
+		data.forEach(message => {
+			let messageJson = JSON.parse(message);
+			if(messageJson.broadcast){
+				messageJson = messageJson.broadcast;
+				let { eventName } = messageJson;
+				callEvent(eventName, messageJson, true);
+			}else{
+				let { eventName } = messageJson;
+				callEvent(eventName, messageJson);
+			}
+		});
+	});
+
+	function callEvent(eventName, data, broadcast = false){
+		switch(eventName){
+			case 'userJoin':
+				if(broadcast){
+					socket.broadcast.emit('user join', data.name);
+				}
+				break;
+			case 'usersUpdate':
+				io.emit('users update', data.users);
+				break;
+			case 'userDisconnectedJson':
+				if(broadcast){
+					socket.broadcast.emit('user disconnect', data.name);
+				}
+				break;
+			case 'userMessage':
+				if(broadcast){
+					socket.broadcast.emit('user msg', data.name, { body: data.text, senderId: data.socketId });
+				}
+				break;
+			case 'userStopTyping':
+				if(broadcast){
+					socket.broadcast.emit('stopping typing');
+				}
+				break;
+			case 'userTyping':
+				if(broadcast){
+					socket.broadcast.emit('user typing', data.name);
+				}
+				break;
+			case 'stopTyping':
+				if(broadcast){
+					socket.broadcast.emit('user stopped typing');
+				}
+				break;
+			default:
+				console.log("Evento não encontrado: ", message.eventName);
+				break;
+		}
+	}
+
+	clientTcp.on('close', function() {
+		console.log('Connection closed');
+	});
+
     console.log('New connection', socket.id);
 
 	socket.on('join', (name) => {
         console.log('-={', name);
-		users[socket.id] = name;
-		socket.broadcast.emit('user join', users[socket.id]);
-		io.emit('users update', users);
+		clientTcp.write(JSON.stringify({
+			eventName: "connect",
+			socketId: socket.id,
+			name: name.name
+		}));
 
 		socket.on('disconnect', () => {
             console.log('-={ disconnect');
-			socket.broadcast.emit('user disconnect', users[socket.id]);
-			delete users[socket.id];
-			io.emit('users update', users);
+			clientTcp.write(JSON.stringify({
+				eventName: "disconnect",
+				socketId: socket.id
+			}));
 		})
 
 		socket.on("msg", (msg) => {
             console.log('-={ msg ', msg);
-			socket.broadcast.emit('user msg', users[socket.id], msg);
-			socket.broadcast.emit('stopping typing');
+			clientTcp.write(JSON.stringify({
+				eventName: "msg",
+				socketId: socket.id,
+				text: msg.body
+			}));
 		});  
 
 		socket.on('typing', () => {
-			socket.broadcast.emit('user typing', users[socket.id]);
+			clientTcp.write(JSON.stringify({
+				eventName: "typing",
+				socketId: socket.id
+			}));
 		});
 		
 		socket.on('stopping typing', () => {
-			socket.broadcast.emit('user stopped typing');
+			clientTcp.write(JSON.stringify({
+				eventName: "stopTyping",
+				socketId: socket.id
+			}));
 		});
 	});
 
